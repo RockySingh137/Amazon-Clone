c187579fdc75077e84412310cd5fbb35326a9b40
Tusshar <tpaul.be21@gmail.com>
2022-10-30 22:42:10 +0530

    added sign up page

--- client/src/components/signup_signin/Sign_up.js
+++ client/src/components/signup_signin/Sign_up.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Sign_up = () => {
   return (
-    <div>Sign up</div>
+    <>
+    <section>
+         <div className="sign_container">
+         <div className="sign_header">
+         <img src="./blacklogoamazon.png" alt="signupimg" />
+          </div>
+          <div className="sign_form">
+          <form >
+          <h1>Sign-In</h1>
+          <div className="form_data">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email"
+                        onChange={adddata}
+                        value={logdata.email}
+                        id="email" />
+                </div>
+                <div className="form_data">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password"
+                        onChange={adddata}
+                        value={logdata.password}
+                        id="password" placeholder="At least 6 characters" />
+                </div>
+                <button type="submit" className="signin_btn" onClick={senddata}>Continue</button>
+          </form>
+          </div>
+          <div className="create_accountinfo">
+          <p>New to Amazon?</p>
+          <button>  <NavLink to="/signup">Create your Amazon Account</NavLink></button>
+      </div>
+         </div>
+    </section>
+</>
   )
 }
 
